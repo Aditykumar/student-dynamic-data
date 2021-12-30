6402e567d43ec0accd9fd425446767062e588419
Aditykumar <adityakushwah29@gmail.com>
2021-12-31 03:19:57 +0530

    done

--- src/Components/StudentTable.js
+++ src/Components/StudentTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useContext } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,7 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react/cjs/react.development';
 import { StudentContext } from './StudentContext';
 
 function Student() {
